5fea84bf101f0051914c2186a86ee7e9d4474bd3
Mark Tellez <mark.tellez@icloud.com>
2020-09-05 16:00:34 -0500

    genericized list

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,7 +4,19 @@ import "./styles.css";
 
 import screencasts from "../data/screencasts.json";
 import List from "./list";
+import Screencast from "./screencast";
 
 export default function App() {
-  return <List items={screencasts} />;
+  return (
+    <List
+      items={screencasts}
+      id='ext_id'
+      render={(screencast) => (
+        <Screencast
+          ext_id={screencast.ext_id}
+          title={screencast.title}
+        />
+      )}
+    />
+  );
 }
--- src/app/list/index.jsx
+++ src/app/list/index.jsx
@@ -1,24 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function List({ items }) {
+export default function List({ items, id, render }) {
   return (
     <ul>
       {items.map((item) => (
-        <li>
-          <div className='__dml_list_item'>
-            <a
-              href={`https://www.youtube.com/watch?v=${item.ext_id}`}
-            >
-              <img
-                src={`/thumbs/${item.ext_id}.png`}
-                width='100%'
-                alt={item.title}
-              />
-              <div>{item.title}</div>
-            </a>
-          </div>
-        </li>
+        <li key={item[id]}>{render(item)}</li>
       ))}
     </ul>
   );
 }
+
+List.propTypes = {
+  render: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+};
