0ae0919fdf02875f6dbd74ff0094d64b2e03d711
Mark Tellez <mark.tellez@icloud.com>
2020-09-05 15:53:41 -0500

    listing screencasts

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,6 +2,29 @@ import React from "react";
 
 import "./styles.css";
 
+import screencasts from "../data/screencasts.json";
+
 export default function App() {
-  return <div>Start here</div>;
+  return (
+    <div>
+      <ul>
+        {screencasts.map((screencast) => (
+          <li>
+            <div className='__dml_list_item'>
+              <a
+                href={`https://www.youtube.com/watch?v=${screencast.ext_id}`}
+              >
+                <img
+                  src={`/thumbs/${screencast.ext_id}.png`}
+                  width='100%'
+                  alt={screencast.title}
+                />
+                <div>{screencast.title}</div>
+              </a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
