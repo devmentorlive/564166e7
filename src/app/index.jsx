ca5203fd37b0560339956cdbeb2631233a64a093
Mark Tellez <mark.tellez@icloud.com>
2020-09-05 16:04:57 -0500

    composable filter

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,17 +4,32 @@ import "./styles.css";
 
 import screencasts from "../data/screencasts.json";
 import List from "./list";
+import Filter from "./filter";
+
 import Screencast from "./screencast";
 
 export default function App() {
+  function filterScreencasts(query, screencasts) {
+    return screencasts.filter(
+      (screencast) =>
+        screencast.title.toLowerCase().indexOf(query.toLowerCase()) >
+        -1
+    );
+  }
+
   return (
-    <List
-      items={screencasts}
-      id='ext_id'
-      render={(screencast) => (
-        <Screencast
-          ext_id={screencast.ext_id}
-          title={screencast.title}
+    <Filter
+      placeholder='Type to filter the screencasts'
+      render={(query) => (
+        <List
+          items={filterScreencasts(query, screencasts)}
+          id='ext_id'
+          render={(screencast) => (
+            <Screencast
+              ext_id={screencast.ext_id}
+              title={screencast.title}
+            />
+          )}
         />
       )}
     />
