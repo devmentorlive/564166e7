c273d78f59984d381b88b89ecc35b464e30b9070
Mark Tellez <mark@devmentor.live>
2020-09-06 02:28:27 -0500

    Update index.jsx

--- src/app/text-field/index.jsx
+++ src/app/text-field/index.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 
 export default function TextField({
   placeholder,
@@ -7,33 +6,15 @@ export default function TextField({
   value,
   onChange,
 }) {
-  const [_value, _setValue] = useState(value);
-
-  useEffect(() => {
-    _setValue(value);
-  }, [value]);
-
   return (
     <div>
       <input
         type={type}
         placeholder={placeholder}
         className='__dml_text-field'
-        value={_value}
-        onChange={(e) => {
-          _setValue(e.target.value);
-          if (onChange && typeof onChange === "function")
-            onChange(e.target.value, e);
-        }}
+        value={value}
+        onChange={(e) => onChange(e.target.value, e)}
       />
     </div>
   );
 }
-
-TextField.propTypes = {
-  placeholder: PropTypes.string.isRequired,
-  type: PropTypes.oneOf("text", "password", "email", "number")
-    .isRequired,
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
